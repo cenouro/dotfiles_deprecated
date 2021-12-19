f99edb781295757774a5a41ebf6e29b389ddcd69
Mario Maia <cenouro+git@protonmail.com>
2021-12-19 18:55:03 -0300

    nvim: Create basic init.lua configuration
    Options have been configured using the same values as those used in
    vimrc. Omitted options already had the desired values by default.

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.cmd [[autocmd BufWritePre * :%s/\s\+$//e]]
+
+local opt = vim.opt
+opt.cursorline = true
+opt.diffopt:append({'algorithm:patience', 'indent-heuristic'})
+opt.fileencoding = 'utf-8'
+opt.list = true
+opt.number = true; opt.relativenumber = true
+opt.smartindent = true
+opt.swapfile = false; opt.backup = false
+opt.tabstop = 4; opt.shiftwidth = 4; opt.softtabstop = 4; opt.expandtab = true
+opt.termguicolors = true
+
